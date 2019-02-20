875bd891b5761f807e7c5ae2d350801a5420453d
Ilya Krasnovsky <IKrasnovsky@corp.idtus.com>
2019-02-20 11:47:26 -0500

    cherry-picked changes from 1.66.0/testing to 1.66.0/stable to see if we can get a centos build

--- conanfile.py
+++ conanfile.py
@@ -15,8 +15,8 @@ class BoostPythonConan(ConanFile):
     lib_short_names = ["python"]
     is_header_only = False
     
-    options = {"shared": [True, False], "python": "ANY"}
-    default_options = "shared=False", "python=python"
+    options = {"shared": [True, False], "python_version": "ANY"}
+    default_options = "shared=False"
 
     source_only_deps = ["graph", "multi_index", "parameter", 
     "property_map", "serialization", "unordered"]
@@ -43,70 +43,19 @@ class BoostPythonConan(ConanFile):
     )
 
     def package_info_additional(self):
-        self.cpp_info.includedirs.append(self.python_include)
-        self.cpp_info.libdirs.append(os.path.dirname(self.python_lib))
-        self.cpp_info.libs.append(os.path.basename(self.python_lib))
+        if self.options.shared:
+            self.cpp_info.defines.append('BOOST_PYTHON_DYNAMIC_LIB')
+        else:
+            self.cpp_info.defines.append('BOOST_PYTHON_STATIC_LIB')
             
     def package_id_additional(self):
-        self.info.options.python = "python-" + self.python_version
-
         boost_deps_only = [dep_name for dep_name in self.info.requires.pkg_names if dep_name.startswith("boost_")]
         for dep_name in boost_deps_only:
             self.info.requires[dep_name].full_version_mode()
-
-    def _is_amd64_to_i386(self):
-        return self.settings.arch == "x86" and tools.detected_architecture() == "x86_64"
-
-    def system_requirements(self):
-        pass
     
-    @property
-    def python_exec(self):
-        try:
-            pyexec = str(self.options.python)
-            output = StringIO()
-            self.run('{0} -c "import sys; print(sys.executable)"'.format(pyexec), output=output)
-            return output.getvalue().strip().replace("\\", "/")
-        except:
-            return ""
-
-    @property
-    def python_include(self):
-        pyinclude = self.get_python_path("include")
-        if not os.path.exists(os.path.join(pyinclude, 'pyconfig.h')):
-            return ""
-        else:
-            return pyinclude.replace('\\', '/')
-
-    @property
-    def python_version(self):
-        cmd = "from sys import *; print('%d.%d' % (version_info[0],version_info[1]))"
-        return self.run_python_command(cmd)
-
-    @property
-    def python_version_nodot(self):
-        cmd = "from sys import *; print('%d%d' % (version_info[0],version_info[1]))"
-        return self.run_python_command(cmd)
-
-    @property
-    def python_lib(self):
-        py_stdlib = self.get_python_path("stdlib").replace('\\', '/')
-        if self.settings.os == "Windows":
-            py_stdlib = os.path.join(os.path.dirname(py_stdlib), "libs", "python"+self.python_version_nodot+".lib")
-        return py_stdlib
-
-    def get_python_path(self, dir_name):
-        cmd = "import sysconfig; print(sysconfig.get_path('{0}'))".format(dir_name)
-        return self.run_python_command(cmd)
-
-    def run_python_command(self, cmd):
-        pyexec = self.python_exec
-        if pyexec:
-            output = StringIO()
-            self.run('{0} -c "{1}"'.format(pyexec, cmd), output=output)
-            return output.getvalue().strip()
-        else:
-            return ""
+    def source_additional(self):
+        if self.options.python_version != self.deps_user_info['python_dev_config'].python_version:
+            raise Exception("Python version does not match with configured python dev, expected %s but got %s." % (self.options.python_version, self.deps_user_info['python_dev_config'].python_version))
 
     # BEGIN
 
